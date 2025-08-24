f7d5923ef80f06d9449ce7f527b4ae38db83a777
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2025-08-24 19:08:22 -0300

    Adding deploy aws lambda sample

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 export const handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello Lambda!", id: uuidv4() }),
+    body: JSON.stringify({ message: "Hello Lambda!!", id: uuidv4() }),
   };
 };
